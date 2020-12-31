751bf06cb94aa1de62a0a36a3481c33856c94493
green1052 <cydyellowgreen@gmail.com>
2021-01-01 03:53:44 +0900

    Add files via upload

---
+++ AdofaiModLoader.Patcher/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace AdofaiModLoader.Patcher
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path;
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Provide the location of Assembly-CSharp.dll:");
+
+                path = Console.ReadLine();
+            }
+            else
+                path = args[0];
+
+            ModuleDefMD module = ModuleDefMD.Load(path);
+
+            if (module == null)
+            {
+                Console.WriteLine($"File {path} not found!");
+                return;
+            }
+
+            Console.WriteLine($"Loaded {module.Name}");
+
+            Console.WriteLine("Resolving References...");
+
+            module.Context = ModuleDef.CreateModuleContext();
+
+            ((AssemblyResolver) module.Context.AssemblyResolver).AddToCache(module);
+
+            Console.WriteLine("Injecting the Bootstrap Class.");
+
+            ModuleDefMD bootstrap =
+                ModuleDefMD.Load(Path.Combine(Directory.GetCurrentDirectory(), "AdofaiModLoader.Bootstrap.dll"));
+
+            Console.WriteLine("Loaded " + bootstrap.Name);
+
+            TypeDef modClass = bootstrap.Types[0];
+
+            foreach (TypeDef type in bootstrap.Types)
+            {
+                if (type.Name == "Bootstrap")
+                {
+                    modClass = type;
+                    Console.WriteLine($"[Injection] Hooked to: \"{type.Namespace}.{type.Name}\"");
+                }
+            }
+
+            TypeDef modRefType = modClass;
+
+            bootstrap.Types.Remove(modClass);
+
+            modRefType.DeclaringType = null;
+
+            module.Types.Add(modRefType);
+
+            MethodDef call = FindMethod(modRefType, "Load");
+
+            if (call == null)
+            {
+                Console.WriteLine($"Failed to get the \"{call.Name}\" method! Maybe you don't have permission?");
+                return;
+            }
+
+            Console.WriteLine("Injected!");
+            Console.WriteLine("Injection completed!");
+            Console.WriteLine("Patching code...");
+
+            TypeDef typeDef = FindType(module.Assembly, "ADOBase");
+
+            MethodDef start = FindMethod(typeDef, "Startup");
+
+            if (start == null)
+            {
+                start = new MethodDefUser("Startup", MethodSig.CreateInstance(module.CorLibTypes.Void),
+                    MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                    MethodAttributes.Private | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+                typeDef.Methods.Add(start);
+            }
+
+            start.Body.Instructions.Insert(4, OpCodes.Call.ToInstruction(call));
+
+            module.Write(Path.Combine(Path.GetDirectoryName(Path.GetFullPath(path)), "Assembly-CSharp-Mod.dll"));
+
+            Console.WriteLine("Patching completed successfully!");
+        }
+
+        private static MethodDef FindMethod(TypeDef type, string methodName)
+        {
+            return type?.Methods.FirstOrDefault(method => method.Name == methodName);
+        }
+
+        private static TypeDef FindType(AssemblyDef assembly, string path)
+        {
+            return assembly.Modules.SelectMany(module => module.Types).FirstOrDefault(type => type.FullName == path);
+        }
+    }
+}
---
+++ AdofaiModLoader.Patcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdofaiModLoader.Patcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdofaiModLoader.Patcher")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8946AEE9-C45D-4D3E-A1B1-509083C037D3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
